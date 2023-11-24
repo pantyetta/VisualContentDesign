9fffbfc1174a03defb128bb22f1488f8501cd486
pantyetta <email@pantyetta.com>
2023-11-24 11:53:10 +0900

    update logo

--- app/header.tsx
+++ app/header.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import Logo from '@/public/logo.png'
+
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -16,7 +18,8 @@ const Header = () => {
   return (
     <header className="sticky w-full border-b-[1px] drop-shadow-sm top-0 bg-white z-10">
       <div className="flex justify-between items-center px-6 mx-auto h-16 container">
-        <Link href="/">
+        <Link href="/" className="flex flex-row items-center">
+          <Image className="w-16 h-16" src={Logo} alt="logo" />
           <p>イタリアン CIT</p>
         </Link>
         <div className="flex flex-row">
